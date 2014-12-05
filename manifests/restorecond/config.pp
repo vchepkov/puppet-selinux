db66b8a4a3cf20f50bf3fc2ce766939fe7fdd188
Vadym Chepkov <vchepkov@gmail.com>
2014-12-05 03:27:44 -0500

    no ensure attribute in concat

--- manifests/restorecond/config.pp
+++ manifests/restorecond/config.pp
@@ -4,7 +4,6 @@
 class selinux::restorecond::config {
 
   concat { $selinux::restorecond::config_file:
-    ensure => present,
     mode   => $selinux::restorecond::config_file_mode,
     owner  => $selinux::restorecond::config_file_owner,
     group  => $selinux::restorecond::config_file_group,
